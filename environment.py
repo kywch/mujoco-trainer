42f67210b23e5fb5675731507f4215dd251746ba
kywch <choe.kyoung@gmail.com>
2024-08-18 17:52:40 -0700

    add rew centering

--- environment.py
+++ environment.py
@@ -9,13 +9,15 @@ import pufferlib.emulation
 import pufferlib.postprocess
 
 
-def single_env_creator(env_name, run_name, capture_video, gamma, idx=None, pufferl=False):
+def single_env_creator(
+    env_name, run_name, capture_video, gamma, idx=None, pufferl=False, center_reward=True
+):
     if capture_video and idx == 0:
         env = gymnasium.make(env_name, render_mode="rgb_array")
         env = gymnasium.wrappers.RecordVideo(env, f"videos/{run_name}")
     else:
         env = gymnasium.make(env_name)
-    env = EpisodeStats(env)
+    env = EpisodeStats(env, center_reward=center_reward)
     env = pufferlib.postprocess.ClipAction(env)  # NOTE: this changed actions space
     env = gymnasium.wrappers.NormalizeObservation(env)
     env = gymnasium.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
@@ -57,11 +59,15 @@ class EpisodeStats(gymnasium.Wrapper):
     """Wrapper for Gymnasium environments that stores
     episodic returns and lengths in infos"""
 
-    def __init__(self, env):
+    def __init__(self, env, center_reward=False):
         self.env = env
         self.observation_space = env.observation_space
         self.action_space = env.action_space
         self.reset()
+
+        self.center_reward = center_reward
+        self.total_reward = 0
+        self.total_steps = 0
 
     def reset(self, seed=None, options=None):
         self.info = dict(episode_return=0, episode_length=0)
@@ -70,11 +76,20 @@ class EpisodeStats(gymnasium.Wrapper):
     def step(self, action):
         observation, reward, terminated, truncated, info = super().step(action)
 
+        # For reward centering, see https://arxiv.org/abs/2405.09999
+        self.total_reward += reward
+        self.total_steps += 1
+        average_reward = self.total_reward / self.total_steps
+
         self.info["episode_return"] += reward
         self.info["episode_length"] += 1
+        self.info["average_reward"] = average_reward
 
         if terminated or truncated:
             for k, v in self.info.items():
-                info[k] = int(v)
+                info[k] = v
+
+        if self.center_reward:
+            reward -= average_reward
 
         return observation, reward, terminated, truncated, info
